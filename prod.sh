dfca20b9e012e1b0669615a6973a82499bc5528f
RackGamesDev <yesterdiscord7@gmail.com>
2025-12-19 15:43:32 +0100

    Arreglo

--- prod.sh
+++ prod.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+cat .env.example > .env
+cd frontend
+cat .env.example > .env
+cd ..
 docker compose -f ./docker-compose-prod.yml down
 docker compose -f docker-compose-prod.yml up --build
